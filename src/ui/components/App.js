ec800ca2a0a4fffc30adf2e4ea0cd902553d9c1b
pbokolo <dev.pbokolo@gmail.com>
2023-03-20 05:48:04 +0200

    Getting real data fom the restcountries api

--- src/ui/components/App.js
+++ src/ui/components/App.js
@@ -1,9 +1,21 @@
+import { useState } from "react";
+import axios from "axios";
 import Country from "./Country";
 import "../style/style.css";
+
 function App() {
+  const [country, setCountry] = useState(null);
+  const fetch = async () => {
+    const result = await axios.get('https://restcountries.com/v3.1/name/DR Congo');
+    const [data] = result.data;
+    setCountry(data)
+  }
   return (
     <div className="app">
-      <Country/>
+      {country && <Country data={country}/>}
+      <button onClick={() => {
+        if(!country)fetch();
+      }} className="btn btn__click-me">Click me</button>
     </div>
   );
 }
--- src/ui/components/Country.js
+++ src/ui/components/Country.js
@@ -1,13 +1,12 @@
 import React from "react";
-import flag from "../../assets/drc.png"
 
-export default function Country(){
+export default function Country({data}){
     return <div className="country">
-        <img src={flag} alt="Country flag" className="country__flag"/>
-        <p className="country__name">Congo DR</p>
-        <p className="country__name">Africa</p>
-        <p className="country__name">100 M</p>
-        <p className="country__name">Swahili, Lingala, Tshiluba, Kikongo, French</p>
-        <p className="country__name">Congolese Franc(CDF)</p>
+        <img src={data.flags.svg} alt={`${data.name.common} flag`} className="country__flag"/>
+        <p className="country__name">{data.name.common}</p>
+        <p className="country__region">{data.region}</p>
+        <p className="country__pop">{data.population}</p>
+        <p className="country__name">{data.languages.fra}</p>
+        <p className="country__currency">{`${data.currencies.CDF.name} (${data.currencies.CDF.symbol})`}</p>
         </div>
 }
