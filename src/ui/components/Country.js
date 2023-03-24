a8d78a60c71f33143e89a6cac1bd5ab825130936
pbokolo <dev.pbokolo@gmail.com>
2023-03-24 21:14:55 +0200

    Moving from react icon to mui-icons from the react-mui library

--- src/ui/components/Country.js
+++ src/ui/components/Country.js
@@ -1,14 +1,18 @@
 import React from "react";
+import PublicOutlinedIcon from '@mui/icons-material/PublicOutlined';
+import EscalatorWarningOutlinedIcon from '@mui/icons-material/EscalatorWarningOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import AttachMoneyOutlinedIcon from '@mui/icons-material/AttachMoneyOutlined';
 
 export default function Country({data}){
     return <div className="country">
         <img src={data.flags.svg} alt={`${data.name.common} flag`} className="country__flag"/>
        
         <p className="country__name">{data.name.common}</p>
-        <p className="country__region">{data.region}</p>
-        <p className="country__pop">{data.population}</p>
-        <p className="country__lang">{data.languages.fra}</p>
-        <p className="country__currency">{`${data.currencies.CDF.name} (${data.currencies.CDF.symbol})`}</p>
+        <p className="country__region"><span className="country__detail"><PublicOutlinedIcon />{data.region}</span></p>
+        <p className="country__pop"><span className="country__detail"><EscalatorWarningOutlinedIcon/>{data.population}</span></p>
+        <p className="country__lang"><span className="country__detail"><LanguageOutlinedIcon/>{data.languages.fra}</span></p>
+        <p className="country__currency"><span className="country__detail"><AttachMoneyOutlinedIcon/>{`${data.currencies.CDF.name} (${data.currencies.CDF.symbol})`}</span></p>
        
         </div>
 }
--- src/ui/components/LocateBtn.js
+++ src/ui/components/LocateBtn.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { MdPersonPinCircle as Location } from 'react-icons/md'
+import MyLocationOutlinedIcon from '@mui/icons-material/MyLocationOutlined';
 
 export default function LocateBtn({clickHandler}){
-    return <button onClick={clickHandler} className="btn btn__locate"><Location className="icon" /></button>
+    return <button onClick={clickHandler} className="btn btn__locate"><MyLocationOutlinedIcon className="icon" /></button>
 }
