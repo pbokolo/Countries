ebd1cc6ff39f07a5e951bc3b681bec9cbbf8c635
pbokolo <dev.pbokolo@gmail.com>
2023-03-24 19:35:06 +0200

    Finnishing up the style of the country card with css grid

--- src/ui/components/Country.js
+++ src/ui/components/Country.js
@@ -3,12 +3,12 @@ import React from "react";
 export default function Country({data}){
     return <div className="country">
         <img src={data.flags.svg} alt={`${data.name.common} flag`} className="country__flag"/>
-        <div className="country__details">
+       
         <p className="country__name">{data.name.common}</p>
         <p className="country__region">{data.region}</p>
         <p className="country__pop">{data.population}</p>
         <p className="country__lang">{data.languages.fra}</p>
         <p className="country__currency">{`${data.currencies.CDF.name} (${data.currencies.CDF.symbol})`}</p>
-        </div>
+       
         </div>
 }
