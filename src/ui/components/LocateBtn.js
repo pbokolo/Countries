6ddb9e2ac7b0acc524cad3756cfe0f5a2aac33c2
pbokolo <dev.pbokolo@gmail.com>
2023-03-24 20:33:26 +0200

    Enhancing the locate button style

--- src/ui/components/LocateBtn.js
+++ src/ui/components/LocateBtn.js
@@ -1,5 +1,6 @@
 import React from "react";
+import { MdPersonPinCircle as Location } from 'react-icons/md'
 
 export default function LocateBtn({clickHandler}){
-    return <button onClick={clickHandler} className="btn btn__locate">L</button>
+    return <button onClick={clickHandler} className="btn btn__locate"><Location className="icon" /></button>
 }
