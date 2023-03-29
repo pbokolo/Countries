fbf4025eca832212c33d2c804d4e6f308216a53b
pbokolo <dev.pbokolo@gmail.com>
2023-03-29 06:13:23 +0200

    Enhancing app bar style

--- src/ui/components/AppBar.js
+++ src/ui/components/AppBar.js
@@ -1,9 +1,9 @@
 import React from "react";
 import SearchForm from "./SearchForm";
 
-export default function AppBar({ error, submitHandler }) {
+export default function AppBar({ error, submitHandler, scroll }) {
   return (
-    <div className="app__bar">
+    <div className={"app__bar"}>
       <SearchForm error={error} submitHandler={submitHandler} />
     </div>
   );
