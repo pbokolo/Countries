50810c82b8eadd4b9df53f7755458be0f70731c9
pbokolo <dev.pbokolo@gmail.com>
2023-03-29 05:30:33 +0200

    Minor style and data sorting

--- src/controller/countries.js
+++ src/controller/countries.js
@@ -1,19 +1,29 @@
 import axios from "axios";
 const API = "https://restcountries.com/v3.1";
 
-const fetchCountry = async(name = "DR Congo") => {
-    const result = await axios.get(`${API}/name/${name}`);
-    if(!result) throw new Error(`Country ${name} not found`);
-    const [data] = result.data;
-    return data; 
-}
+const fetchCountry = async (name = "DR Congo") => {
+  const result = await axios.get(`${API}/name/${name}`);
+  if (!result) throw new Error(`Country ${name} not found`);
+  const [data] = result.data;
+  return data;
+};
 
-const fetchAll = async() => {
-    const result = await axios.get(`${API}/all`);
-    if(!result) throw new Error(`Country not found`);
-    const {data} = result;
-    return data; 
-}
+const fetchAll = async () => {
+  const result = await axios.get(`${API}/all`);
+  if (!result) throw new Error(`Country not found`);
+  let { data } = result;
+  data = data.sort((a, b) => {
+    const nameA = a.name.common; // ignore upper and lowercase
+    const nameB = b.name.common; // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
+  return data;
+};
 
-
-export {fetchCountry, fetchAll}
+export { fetchCountry, fetchAll };
