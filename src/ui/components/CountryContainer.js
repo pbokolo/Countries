cfb633486e3372d4d58af23cd3cd4db2bb843740
pbokolo <dev.pbokolo@gmail.com>
2023-03-30 07:25:47 +0200

    Setting up the loader

--- src/ui/components/CountryContainer.js
+++ src/ui/components/CountryContainer.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Country from "./Country";
+import Loader from "./Loader";
 
 export default function CountryContainer({ countries }) {
   return (
     <div className="container">
-      {countries.map((country, index) => (
-        <Country key={index} data={country} />
-      ))}
+      {countries ? (
+        countries.map((country, index) => (
+          <Country key={index} data={country} />
+        ))
+      ) : (
+        <Loader />
+      )}
     </div>
   );
 }
--- src/ui/components/index.js
+++ src/ui/components/index.js
@@ -3,5 +3,6 @@ import Country from "./Country";
 import SearchForm from "./SearchForm";
 import CountryContainer from "./CountryContainer";
 import AppBar from "./AppBar";
+import Loader from "./Loader";
 
-export { LocateBtn, Country, SearchForm, CountryContainer, AppBar };
+export { LocateBtn, Country, SearchForm, CountryContainer, AppBar, Loader };
