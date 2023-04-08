6ab23b1e9fc753d8f5a45032b2e426fb863c6f01
pbokolo <dev.pbokolo@gmail.com>
2023-04-08 07:29:42 +0200

    Updating the style and layout of the country details dialog

--- src/ui/components/DetailsDialog.js
+++ src/ui/components/DetailsDialog.js
@@ -1,10 +1,19 @@
 import React from "react";
+import CloseIcon from "@mui/icons-material/Close";
+import LocationCityIcon from "@mui/icons-material/LocationCity";
+import SouthAmericaIcon from "@mui/icons-material/SouthAmerica";
+import TranslateIcon from "@mui/icons-material/Translate";
+import GroupsIcon from "@mui/icons-material/Groups";
 
 export default function DetailsDialog({ data, closeHandler }) {
+  let languages = Object.values(data.languages);
+  let neighbors = Object.values(data.borders);
   return (
     <div className="dialog dialog__overlay" onClick={closeHandler}>
       <div className="dialog__container">
-        <span className="dialog__close">X</span>
+        <span className="dialog__close">
+          <CloseIcon className="dialog__close-icon" />
+        </span>
         <div className="flag__container">
           <img
             src={data.flags.png}
@@ -12,7 +21,33 @@ export default function DetailsDialog({ data, closeHandler }) {
             className="country__flag"
           />
         </div>
-        <div className="details__container">Details</div>
+        <div className="details__container">
+          <p className="country__region">
+            <span className="country__detail">
+              <LocationCityIcon />
+              {data.capital}
+            </span>
+          </p>
+
+          <p className="country__region">
+            <span className="country__detail">
+              <SouthAmericaIcon />
+              {data.subregion}
+            </span>
+          </p>
+          <p className="country__region">
+            <span className="country__detail">
+              <TranslateIcon />
+              {`${languages}`}
+            </span>
+          </p>
+          <p className="country__region">
+            <span className="country__detail">
+              <GroupsIcon />
+              {`${neighbors}`}
+            </span>
+          </p>
+        </div>
       </div>
     </div>
   );
