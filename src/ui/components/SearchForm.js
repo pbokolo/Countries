df17ce652e5ee0291b100cd3d7b839d3aeeeb89f
pbokolo <dev.pbokolo@gmail.com>
2023-04-21 06:44:11 +0200

    Moving search handler in the SearchForm component

--- src/ui/components/SearchForm.js
+++ src/ui/components/SearchForm.js
@@ -1,16 +1,34 @@
 import React, { useState } from "react";
 
-import TextInput from "./TextInput";
+import { useDispatch } from "react-redux";
 
-export default function SearchForm({ submitHandler, error }) {
+import TextInput from "./TextInput";
+import { Countries } from "../../controller/countries";
+
+export default function SearchForm() {
+  const controller = new Countries(useDispatch);
+  const [error, setError] = useState("");
   const [searchKeyWord, setSearchKeyword] = useState("");
   const handleChange = (e) => setSearchKeyword(e.target.value);
+  /**
+   *
+   * @param {Event object} e
+   * @param {Country name} keyWord
+   */
+  const handleSearch = async (e, keyWord) => {
+    e.preventDefault();
+    try {
+      await controller.fetchByName(keyWord, setError);
+    } catch (error) {
+      setError(error.response.data.message);
+    }
+  };
 
   return (
     <form
       className="form"
       onSubmit={(e) => {
-        submitHandler(e, searchKeyWord);
+        handleSearch(e, searchKeyWord);
         setSearchKeyword("");
       }}
     >
--- src/ui/pages/Home.js
+++ src/ui/pages/Home.js
@@ -7,22 +7,7 @@ import { CountryContainer, AppBar, DetailsDialog } from "../components";
 
 export default function Home() {
   const controller = new Countries(useDispatch);
-  const [error, setError] = useState("");
   const [showDialog, setShowDialog] = useState(false);
-
-  /**
-   *
-   * @param {Event object} e
-   * @param {Country name} keyWord
-   */
-  const handleSearch = async (e, keyWord) => {
-    e.preventDefault();
-    try {
-      await controller.fetchByName(keyWord, setError);
-    } catch (error) {
-      setError(error.response.data.message);
-    }
-  };
 
   const handleCountryClick = (e) => {
     controller.handleCountryClick(e.target.closest("div").dataset.index);
@@ -36,7 +21,7 @@ export default function Home() {
   };
   return (
     <div className="page page__home">
-      <AppBar error={error} submitHandler={handleSearch} />
+      <AppBar />
       <CountryContainer clickHandler={handleCountryClick} />
 
       {showDialog && <DetailsDialog closeHandler={handleClodeDialog} />}
